d6dbe19a421adebd77f7b5f436ecfed1c0ef013e
Antonin-crypto <akamis53@gmail.com>
2025-01-20 10:02:31 +0100

    PEP8

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,13 +3,14 @@
 This module defines the function minOperations.
 """
 
+
 def minOperations(n):
     """
     Calculate the minimum number of operations to result in exactly n H charac.
     """
     if n < 2:
         return 0
-    
+
     operations = 0
     factor = 2
 
@@ -18,5 +19,5 @@ def minOperations(n):
             operations += factor
             n //= factor
         factor += 1
-    
+
     return operations
