ad7cc09e064bc04fab9e4129aaafd7b8c69ba22d
Antonin-crypto <akamis53@gmail.com>
2025-01-27 15:27:23 +0100

    fix code

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -2,105 +2,76 @@
 #include <stdio.h>
 
 /**
- * print_grid - Prints a 3x3 grid
- * @grid: The grid to be printed
+ * print_grid - Print a 3x3 grid
+ * @grid: The grid to print
  */
 static void print_grid(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    printf("=\n");
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
 
 /**
- * is_stable - Checks if a sandpile is stable
- * @grid: The grid to check
- *
- * Return: 1 if stable, 0 otherwise
+ * apply_topples - Apply toppling operation to a 3x3 grid at position (i, j)
+ * @grid: The grid to apply toppling to
+ * @i: The row index of the current position
+ * @j: The column index of the current position
  */
-static int is_stable(int grid[3][3])
+void apply_topples(int grid[3][3], int i, int j)
 {
-    int i, j;
-
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid[i][j] > 3)
-                return (0);
-        }
-    }
-    return (1);
-}
-
-/**
- * topple - Topples an unstable sandpile
- * @grid: The grid to topple
- */
-static void topple(int grid[3][3])
-{
-    int i, j;
-    int temp[3][3] = {0};
-
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid[i][j] > 3)
-            {
-                temp[i][j] -= 4;
-                if (i > 0)
-                    temp[i - 1][j]++;
-                if (i < 2)
-                    temp[i + 1][j]++;
-                if (j > 0)
-                    temp[i][j - 1]++;
-                if (j < 2)
-                    temp[i][j + 1]++;
-            }
-        }
-    }
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid[i][j] += temp[i][j];
-        }
-    }
+	grid[i][j] -= 4;
+	if (i > 0)
+		grid[i - 1][j]++;
+	if (i < 2)
+		grid[i + 1][j]++;
+	if (j > 0)
+		grid[i][j - 1]++;
+	if (j < 2)
+		grid[i][j + 1]++;
 }
 
 /**
  * sandpiles_sum - Computes the sum of two sandpiles
- * @grid1: The first sandpile (result stored here)
+ * @grid1: The first sandpile
  * @grid2: The second sandpile
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
+	int stable, tmp[3][3], i, j;
 
-    /* Add grid1 and grid2 */
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] += grid2[i][j];
-        }
-    }
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid1[i][j] += grid2[i][j];
+	do {
+		stable = 1;
 
-    /* Stabilize grid1 */
-    while (!is_stable(grid1))
-    {
-        print_grid(grid1);
-        topple(grid1);
-    }
+		for (i = 0; i < 3 && stable; i++)
+			for (j = 0; j < 3 && stable; j++)
+				stable = grid1[i][j] <= 3;
+
+		if (!stable)
+		{
+			printf("=\n");
+			print_grid(grid1);
+
+			for (i = 0; i < 3; i++)
+				for (j = 0; j < 3; j++)
+					tmp[i][j] = grid1[i][j];
+
+			for (i = 0; i < 3; i++)
+				for (j = 0; j < 3; j++)
+					if (tmp[i][j] > 3)
+						apply_topples(grid1, i, j);
+		}
+	} while (!stable);
 }
